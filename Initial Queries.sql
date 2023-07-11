61fc7a60d403f9bbd0c53d038101837d377526e1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-12 01:04:11 +0530

    SQL Inner Query

--- Initial Queries.sql
+++ Initial Queries.sql
@@ -1,3 +1,4 @@
 SELECT ProductID, SUM(Quantity) AS TotalQuantity FROM Orders GROUP BY ProductID;
 SELECT * FROM EMPLOYEES WHERE SALARY = (SELECT DISTINCT(SALARY) FROM EMPLOYEES ORDER BY SALARY DESC LIMIT 1,1);
+SELECT OrderID, CustomerID, OrderDate FROM Orders WHERE CustomerID IN (SELECT CustomerID FROM Customers WHERE LName = 'Ray');
 SELECT CustomerID, SUM(Quantity) AS TotalQuantity FROM Orders WHERE ProductID = 12345 AND OrderDate > '2020-11-11' GROUP BY CustomerID ORDER BY TotalQuantity DESC;
