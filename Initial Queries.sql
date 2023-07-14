9ac7fe612898961190eaa357a0d2a4e1b70bb473
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-14 17:50:48 +0530

    SQL Full Outer Join

--- Initial Queries.sql
+++ Initial Queries.sql
@@ -5,3 +5,4 @@ SELECT CustomerID, SUM(Quantity) AS TotalQuantity FROM Orders WHERE ProductID = 
 SELECT Orders.OrderID, Customers.CustomerName FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
 SELECT Customers.CustomerName, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID ORDER BY Customers.CustomerName;
 SELECT Orders.OrderID, Employees.LastName, Employees.FirstName FROM Orders RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID ORDER BY Orders.OrderID;
+SELECT Customers.CustomerName, Orders.OrderID FROM Customers FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID ORDER BY Customers.CustomerName;
