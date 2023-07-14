1c39c5ce087c2d46bd7f57d928e59ece45cb0edc
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-14 17:48:57 +0530

    SQL Inner Join

--- Initial Queries.sql
+++ Initial Queries.sql
@@ -2,3 +2,4 @@ SELECT ProductID, SUM(Quantity) AS TotalQuantity FROM Orders GROUP BY ProductID;
 SELECT * FROM EMPLOYEES WHERE SALARY = (SELECT DISTINCT(SALARY) FROM EMPLOYEES ORDER BY SALARY DESC LIMIT 1,1);
 SELECT OrderID, CustomerID, OrderDate FROM Orders WHERE CustomerID IN (SELECT CustomerID FROM Customers WHERE LName = 'Ray');
 SELECT CustomerID, SUM(Quantity) AS TotalQuantity FROM Orders WHERE ProductID = 12345 AND OrderDate > '2020-11-11' GROUP BY CustomerID ORDER BY TotalQuantity DESC;
+SELECT Orders.OrderID, Customers.CustomerName FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
