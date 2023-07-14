16fdf41e9c6faefc0b73e3e9817cb6b952706274
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-14 17:49:44 +0530

    SQL Left Join

--- Initial Queries.sql
+++ Initial Queries.sql
@@ -3,3 +3,4 @@ SELECT * FROM EMPLOYEES WHERE SALARY = (SELECT DISTINCT(SALARY) FROM EMPLOYEES O
 SELECT OrderID, CustomerID, OrderDate FROM Orders WHERE CustomerID IN (SELECT CustomerID FROM Customers WHERE LName = 'Ray');
 SELECT CustomerID, SUM(Quantity) AS TotalQuantity FROM Orders WHERE ProductID = 12345 AND OrderDate > '2020-11-11' GROUP BY CustomerID ORDER BY TotalQuantity DESC;
 SELECT Orders.OrderID, Customers.CustomerName FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+SELECT Customers.CustomerName, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID ORDER BY Customers.CustomerName;
