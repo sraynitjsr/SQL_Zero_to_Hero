b1946e498be0535ac133274045507f90782dd4e3
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-16 17:11:55 +0530

    SQL Aggregate Functions

--- first_query.sql
+++ first_query.sql
@@ -1,1 +1,7 @@
-SELECT Day('2017/08/25') AS Day, Month('2017/08/25') AS Month, Year('2017/08/25') AS Year;
+Select Day('2017/08/25') AS Day, Month('2017/08/25') AS Month, Year('2017/08/25') AS Year;
+
+Select Count(*) From Employees Where EmpId >= 20;
+
+Select Sum(Salary) From Employees;
+
+Select Name, Sum(Salary_Components) From Employees Group By Name Having Sum(Salary_Components) >= 150000;
